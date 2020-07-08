2efa0c5fbf492a7ce64ff5b68fe85848eafc4e3f
unhandledrejection <1021679588@qq.com>
2020-07-09 00:31:33 +0800

    fix:  import()

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,5 +1,6 @@
 import { debug } from './debug';
 import { WATER_MARK, TOPIC_FONT_FAMILY } from '../constant';
+import html2canvas from 'html2canvas';
 
 function selectText(el?: HTMLElement) {
   if (!el) return;
@@ -32,18 +33,16 @@ function onClickOutSide(selector: string, callback: Function) {
   };
 }
 
-function htmlToCanvas(el: HTMLElement) {
-  return import('html2canvas').then(async html2canvas => {
-    const canvas = await html2canvas.default(el);
-    const context = canvas.getContext('2d');
-    if (!context) return;
-    context.save();
-    context.font = `20px ${TOPIC_FONT_FAMILY}`;
-    context.fillStyle = '#000';
-    context.fillText(WATER_MARK, canvas.width - 300, canvas.height);
-    context.restore();
-    return canvas.toDataURL();
-  });
+async function htmlToCanvas(el: HTMLElement) {
+  const canvas = await html2canvas(el);
+  const context = canvas.getContext('2d');
+  if (!context) return;
+  context.save();
+  context.font = `20px ${TOPIC_FONT_FAMILY}`;
+  context.fillStyle = '#000';
+  context.fillText(WATER_MARK, canvas.width - 300, canvas.height);
+  context.restore();
+  return canvas.toDataURL();
 }
 
 export { selectText, onClickOutSide, htmlToCanvas };
